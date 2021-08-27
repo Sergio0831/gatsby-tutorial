6785029e2e9599ec3c2f5ac4d3688c3ab7aef41d
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-08-27 08:05:43 +0100

    Created tags page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,10 @@
 import * as React from "react"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
-import { useStaticQuery, graphql } from "gatsby"
 import "normalize.css"
 import "../assets/css/main.css"
 
-const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({ children }) => {
   return (
     <>
       <NavBar />
