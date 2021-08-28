afda4a2e5f75b1f9b2d26276acd993ce7e9a3ddd
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-08-28 08:43:35 +0100

    Updated

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import AllRecipes from "../components/AllRecipes"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/SEO"
 
 const RecipesPage = () => {
